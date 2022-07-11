d447282f3613c3a72791fc31f6bb3d2708c4fdb1
ivonnerubio <ivonnerubiouh@gmail.com>
2022-07-11 18:26:52 -0500

    adding readme and zsh file

--- index.zsh
+++ index.zsh
@@ -3,21 +3,19 @@
 echo -n "Project name: "
 read -r projectName
 
-echo -n "Language: "
-read -r Language
-
-echo "hi + ${Language}"
-
-echo
 echo "Creating Project......."
 
-mkdir "$projectName"
-cd "projectName"
+mkdir $projectName
+cd $projectName
 git init
-git add README.md
+echo "# $projectName" >> README.md
 git add .
 git commit -m "initial commit"
 
+gh repo create $projectName --private
 
+git branch -M main
+git remote add origin https://github.com/ivonnerubio/$projectName.git
+git push -u origin main
 
- curl -u "ivonnerubio" https://api.github.com/user/repos -d '{“name”:"${projectName}, “private”:”true”}'
+code .
